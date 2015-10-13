fc345192bba4cc071deb28fc3145860f4bad23d1
Tuomo Komulainen <tjkomu@utu.fi>
2015-10-13 14:36:58 +0300

    player will always start on grass

--- world.cpp
+++ world.cpp
@@ -6,7 +6,7 @@ World::World(QPoint size)
     : landscape(Landscape::createRandomLandscape(size.x(), size.y()))
     , playerPosition(10,20)
 {
-
+    landscape.setTile(playerPosition, Tile::GRASS_TILE);
 }
 
 World::~World()
