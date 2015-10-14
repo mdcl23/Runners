b3b4b064005bdaa0cc0f46bfd8decb8cfb87b3ed
Tuomo Komulainen <tjkomu@utu.fi>
2015-10-14 05:28:10 +0300

    templates for the brain

--- hexbrain.cpp
+++ hexbrain.cpp
@@ -5,39 +5,98 @@ HexBrain::HexBrain()
 
 }
 
-namespace {
-    int howCloseToWin(const HexGame& game, HexGame::Piece color)
+struct Template {
+    enum TemplatePiece { NoneT, MatchT, OtherT, AnyT };
+
+    int score;
+
+    bool canReverse;
+    bool canTranspose;
+
+    uint size;
+    TemplatePiece templ[9];
+
+    TemplatePiece getPiece(QPoint pt, bool rev, bool trans) const
     {
-        int length = game.size;
+        if (trans) { pt = QPoint(pt.y(), pt.x()); }
+        if (rev) { pt = QPoint(size-1 - pt.x(), pt.y()); }
+        return templ[pt.x() + pt.y()*size];
+    }
 
-        for (int yi = 0; yi < game.size; yi++)
+    bool matchPiece(TemplatePiece tPiece, HexGame::Piece bPiece, HexGame::Piece color) const
+    {
+        if (tPiece == NoneT) {
+            return bPiece == HexGame::NonePiece;
+        } else if (tPiece == MatchT) {
+            return bPiece == color;
+        } else if (tPiece == OtherT) {
+            return (color == HexGame::BlackPiece && bPiece == HexGame::WhitePiece) ||
+                   (color == HexGame::WhitePiece && bPiece == HexGame::BlackPiece);
+        } else if (tPiece == AnyT) {
+            return true;
+        }
+        return false;
+    }
+
+    int matchTemplate(const HexGame& game, QPoint pos, bool reverse, bool transpose, HexGame::Piece color) const
+    {
+        for (uint yi = 0; yi < this->size; yi++)
         {
-            for (int xi = 0; xi < game.size; xi++)
+            for (uint xi = 0; xi < this->size; xi++)
             {
-
+                HexGame::Piece bPiece = game.getPiece(pos + QPoint(xi, yi));
+                TemplatePiece tPiece = this->getPiece(QPoint(xi, yi), reverse, transpose);
+                if (!matchPiece(tPiece, bPiece, color))
+                {
+                    return 0;
+                }
             }
         }
-
-        return 1;
-
+        return this->score;
     }
-}
 
+    int match(const HexGame& game, bool reverse, bool transpose, HexGame::Piece color) const
+    {
+        int matchScore = 0;
+        for (uint xi = 0; xi < game.size - this->size; xi++)
+        {
+            for (uint yi = 0; yi < game.size - this->size; yi++)
+            {
+                matchScore += matchTemplate(game, QPoint(xi, yi), reverse, transpose, color);
+            }
+        }
+        return matchScore;
+    }
+};
 
 int HexBrain::score(const HexGame &game)
 {
-    enum Direction { TopBottom, LeftRight };
 
+    // rev, no trans
+    // +0
+    // XO
+    // OX
 
+    // rev, trans
+    // +1
+    // XX
+    // X?
 
+    // rev, no trans
+    // +1
+    // X.
+    // .X
 
-    // hypothesis TopBottom
+    // rev, trans
+    // +1
+    // ?XX
+    // ?..
+    // ?XX
 
-    // closest to top edge
-
-    // closest to left edge
-
-    // hypothesis LeftRight
+    // no rev, trans
+    // ?X?
+    // ?X?
+    // ?X?
 
     return 1;
 }
